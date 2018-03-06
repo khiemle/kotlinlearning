320d14ccd0d44bef82915311c946f2417ba6b771
Khiem Le <khiemle@Khiems-MacBook-Pro.local>
2018-03-06 17:06:49 +0700

    [f] Model of ViewModel will be lost data when rotate screen

--- app/src/main/java/com/mingle/kotlinlearning/views/MainActivity.kt
+++ app/src/main/java/com/mingle/kotlinlearning/views/MainActivity.kt
@@ -1,25 +1,25 @@
-package com.mingle.kotlinlearning
+package com.mingle.kotlinlearning.views
 
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import com.mingle.kotlinlearning.BR
+import com.mingle.kotlinlearning.R
 import com.mingle.kotlinlearning.databinding.ActivityMainBinding
-import com.mingle.kotlinlearning.models.User
+import com.mingle.kotlinlearning.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private var a : String? = null
-    private var user : User? = null
-    private var binding: ActivityMainBinding? = null
+    private val mainViewModel = MainViewModel()
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        user = User(name = "Le Quang Khiem", age = 23, email = "khiemlq@gmail.com", company = "Mingle")
-        binding?.setVariable(BR.user, user)
-        binding?.executePendingBindings()
+        binding.setVariable(BR.mainviewmodel, mainViewModel)
 
 //        binding?.apply {
 //            edUserName.setText("Nguyen Thi Hoang Sa")
@@ -35,9 +35,7 @@ class MainActivity : AppCompatActivity() {
     fun exampleFun() {
         Log.d("===", a?.length.toString())
         Handler().postDelayed({
-            user?.name="New Name"
-            user?.company="Mingle234"
-            binding?.executePendingBindings()
+            binding.executePendingBindings()
 
         }, 2000)
 
