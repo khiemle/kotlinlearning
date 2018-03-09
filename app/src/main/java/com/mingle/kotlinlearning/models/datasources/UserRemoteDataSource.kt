89b27d16402c9f93ce959afa5094116b79201a44
Khiem Le <khiemle@Khiems-MacBook-Pro.local>
2018-03-09 14:57:52 +0700

    [f] saveUsers: will create Observable with Completable

--- app/src/main/java/com/mingle/kotlinlearning/models/datasources/UserRemoteDataSource.kt
+++ app/src/main/java/com/mingle/kotlinlearning/models/datasources/UserRemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.mingle.kotlinlearning.models.datasources
 
 import com.mingle.kotlinlearning.models.User
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 import java.util.concurrent.TimeUnit
 
 class UserRemoteDataSource : UserDataSource() {
@@ -9,5 +11,10 @@ class UserRemoteDataSource : UserDataSource() {
         val listUsers = ArrayList<User>()
         (1..10).mapTo(listUsers) { User(name = "User $it", age = 29, email = "user$it@gmail.com", company = "Mingle") }
         return Observable.just(listUsers).delay(3, TimeUnit.SECONDS)
+    }
+
+    fun saveUsers(listUsers: ArrayList<User>) : Completable {
+        // Do something with listUsers
+        return Single.just(true).delay(1, TimeUnit.SECONDS).toCompletable()
     }
 }
