74a3e9caf44f9d991a5a9a217ddea3f7d9f266dc
Khiem Le <khiemle@Khiems-MacBook-Pro.local>
2018-03-09 11:12:40 +0700

    [f] dispose ViewModel Observer when destroy activity

--- app/src/main/java/com/mingle/kotlinlearning/viewmodels/UsersViewModel.kt
+++ app/src/main/java/com/mingle/kotlinlearning/viewmodels/UsersViewModel.kt
@@ -15,11 +15,12 @@ class UsersViewModel(context: Application) : AndroidViewModel(context) {
     private val userRepository = UserRepository(NetManager(context))
     var users = MutableLiveData<ArrayList<User>>()
     val isLoading = ObservableField(false)
-
+    lateinit var disposable: Disposable
     fun getListUsers() {
         isLoading.set(true)
         userRepository.getUsers().subscribe(object : Observer<ArrayList<User>> {
             override fun onSubscribe(d: Disposable) {
+                disposable = d
             }
 
             override fun onComplete() {
@@ -34,5 +35,12 @@ class UsersViewModel(context: Application) : AndroidViewModel(context) {
             }
 
         })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if (!disposable.isDisposed) {
+            disposable.dispose()
+        }
     }
 }
